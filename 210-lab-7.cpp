046a70bff00a498dd191be91b0f9e78a4e247e2a
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-12 17:21:25 -0700

    defiend displayArray

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -4,19 +4,29 @@
 using namespace std;
 
 const int SIZE = 5;
-string* reverseArray(string* arr);
+//string* reverseArray(string* arr);
 void displayArray(string* arr);
 
 int main() {
     string* arrPtr = new string[SIZE] {"Janet", "Jeff", "Jin", "Joe", "Junio"};
+    cout << "Original array: "; 
+    displayArray(arrPtr); 
+    cout << "Reversed array: "; 
+    displayArray(arrPtr);
     delete [] arrPtr; 
     return 0; 
 }
 
-string* reverseArray(string* arr) { 
+//string* reverseArray(string* arr) { 
+//
+//}
 
-}
-
+// displayArray(string* arr) outputs content of dynamic double array to console 
+// arguments: string* arr
+// returns: None
 void displayArray(string* arr) { 
-
+    for(int i = 0; i < SIZE; i++) { 
+        cout << arr[i] << " ";
+    }
+    cout << endl; 
 }
