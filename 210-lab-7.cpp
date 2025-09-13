70f6b7b3f9ec0d765799a6921d0aabbbf6b0eeef
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-12 17:36:09 -0700

    Completed code

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -4,22 +4,30 @@
 using namespace std;
 
 const int SIZE = 5;
-//string* reverseArray(string* arr);
+string* reverseArray(string* arr);
 void displayArray(string* arr);
 
 int main() {
     string* arrPtr = new string[SIZE] {"Janet", "Jeff", "Jin", "Joe", "Junio"};
     cout << "Original array: "; 
     displayArray(arrPtr); 
+    arrPtr = reverseArray(arrPtr);
     cout << "Reversed array: "; 
     displayArray(arrPtr);
     delete [] arrPtr; 
     return 0; 
 }
 
-//string* reverseArray(string* arr) { 
-//
-//}
+// reverseArray(string* arr) reverses content of dynamic array
+// arguments: string* arr
+// returns: pointer to the reversed array 
+string* reverseArray(string* arr) { 
+    string* revArray = new string[SIZE]; 
+    for (int i = 0; i < SIZE; i++) { 
+        revArray[i] = arr[SIZE - 1 - i];
+    }
+    return revArray;
+}
 
 // displayArray(string* arr) outputs content of dynamic double array to console 
 // arguments: string* arr
